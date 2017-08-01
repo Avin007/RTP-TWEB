bcc31197f49291c585cf52730334d05d81917a81
Ravo <rasami_r@etna-alternance.net>
2017-08-01 12:11:59 +0100

    second commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connection.on('error',(err)=>{
 
 const app=express();
 const users = require('./routes/users');
-const port = 3000
+const port = process.env.PORT || 8080;
 
 //CORS Middleware
 app.use(cors());
@@ -43,6 +43,10 @@ app.use('/users',users);
 //Index Route
 app.get('/', (req,res)=>{
     res.send('Invalid Endpoint');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/index.html'));
 });
 
 //Start Server
