b55cd011c0416a12560e79bef5fdfc941e261495
Ravo <rasami_r@etna-alternance.net>
2017-08-09 10:36:48 +0100

    deploiement

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ mongoose.connection.on('error',(err)=>{
 const app = express();
 const users = require('./routes/users');
 const deploiements = require('./routes/deploiements');
-const port = 3000;
-//const port = process.env.PORT || 8080;
+//const port = 3000;
+const port = process.env.PORT || 8080;
 
 //CORS Middleware
 app.use(cors());
